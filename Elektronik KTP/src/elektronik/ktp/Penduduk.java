1252e8e7a388dcc3d8d29ef3833f86b68b5c7dda
irfan232 <kusirfan99@gmail.com>
2017-05-19 21:47:44 +0700

    Create Penduduk.java

---
+++ Elektronik KTP/src/elektronik/ktp/Penduduk.java
@@ -0,0 +1,113 @@
+package elektronik.ktp;
+
+public class Penduduk {
+    private String nik,tempatTtglLahir, nama, jenisKelamin, alamat, rtRw, kelDesa, kecamatan, agama, stKawin
+            , pekerjaan, wargaNegara, berlakuHingga;
+
+    public Penduduk() {
+    }
+
+    public String getAgama() {
+        return agama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getBerlakuHingga() {
+        return berlakuHingga;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public String getKecamatan() {
+        return kecamatan;
+    }
+
+    public String getKelDesa() {
+        return kelDesa;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNik() {
+        return nik;
+    }
+
+    public String getPekerjaan() {
+        return pekerjaan;
+    }
+
+    public String getRtRw() {
+        return rtRw;
+    }
+
+    public String getStKawin() {
+        return stKawin;
+    }
+
+    public String getTempatTtglLahir() {
+        return tempatTtglLahir;
+    }
+
+    public String getWargaNegara() {
+        return wargaNegara;
+    }
+
+    public void setAgama(String agama) {
+        this.agama = agama;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public void setBerlakuHingga(String berlakuHingga) {
+        this.berlakuHingga = berlakuHingga;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public void setKecamatan(String kecamatan) {
+        this.kecamatan = kecamatan;
+    }
+
+    public void setKelDesa(String kelDesa) {
+        this.kelDesa = kelDesa;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+
+    public void setPekerjaan(String pekerjaan) {
+        this.pekerjaan = pekerjaan;
+    }
+
+    public void setRtRw(String rtRw) {
+        this.rtRw = rtRw;
+    }
+
+    public void setStKawin(String stKawin) {
+        this.stKawin = stKawin;
+    }
+
+    public void setTempatTtglLahir(String tempatTtglLahir) {
+        this.tempatTtglLahir = tempatTtglLahir;
+    }
+
+    public void setWargaNegara(String wargaNegara) {
+        this.wargaNegara = wargaNegara;
+    }
+}
